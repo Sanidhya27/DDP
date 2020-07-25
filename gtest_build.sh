d597ecd5bdf24b526056555e6e1776d70bc81ad2
Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
2020-07-25 12:01:26 +0530

    Rework sudo again

--- gtest_build.sh
+++ gtest_build.sh
@@ -1,6 +1,6 @@
 cd /usr/src/gtest
-sudo -i cmake CMakeLists.txt
-sudo -i make
-sudo cp *.a /usr/lib
-sudo ln -s /usr/lib/libgtest.a /usr/local/lib/gtest/libgtest.a
-sudo -i ln -s /usr/lib/libgtest_main.a /usr/local/lib/gtest/libgtest_main.a
+cmake CMakeLists.txt
+make
+cp *.a /usr/lib
+ln -s /usr/lib/libgtest.a /usr/local/lib/gtest/libgtest.a
+ln -s /usr/lib/libgtest_main.a /usr/local/lib/gtest/libgtest_main.a
