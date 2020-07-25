b4fc77d61fb598a2f31fe880b1988676e9abcb82
Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
2020-07-25 12:17:59 +0530

    Fix gtest directory creation

--- gtest_build.sh
+++ gtest_build.sh
@@ -2,5 +2,6 @@ cd /usr/src/gtest
 cmake CMakeLists.txt
 make
 cp *.a /usr/lib
+mkdir -p /usr/local/lib/gtest
 ln -s /usr/lib/libgtest.a /usr/local/lib/gtest/libgtest.a
 ln -s /usr/lib/libgtest_main.a /usr/local/lib/gtest/libgtest_main.a
